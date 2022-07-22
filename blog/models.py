70851d8bde73d964040231166ea0cbe45dd7698d
Pascaline Boireau <pascaline_vigouroux@hotmail.com>
2022-07-22 15:41:13 +0200

    second commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Ticket(models.Model):  # ticket model
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):  # ticket model
+    ticket = models.ForeignKey(to=Ticket,on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(max_length=1024,
+                                              validators=[ MinValueValidator(0),
+                                                           MaxValueValidator(5)])
+    content = models.CharField(max_length=5000)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length = 8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
